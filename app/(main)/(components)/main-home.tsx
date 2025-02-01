072d8f2fca0b659427a6f3d80e93007c1afb36ff
Oleksii <alexey.bortnytskyi@gmail.com>
2025-02-01 19:46:26 +0100

    feature(main-home) - added <subtitle> current text <I'm...>

--- app/(main)/(components)/main-home.tsx
+++ app/(main)/(components)/main-home.tsx
@@ -12,7 +12,10 @@ const texts = [
 const MHome = () => {
   return (
     <div className="h-dvh pb-20 flex container mx-auto items-end">
-      <div className="flex -space-y-10 flex-col">
+      <div className="flex -space-y-10 flex-col w-full max-w-5xl items-start border border-input p-10 rounded-xl">
+        <p className="text-xl font-semibold font-sans leading-10 mb-5">
+          Hello there 🖖 I'm Oleksii Bortnytskyi and I'm
+        </p>
         <div className="flex items-center gap-4">
           <span className="text-center font-sans font-bold text-black text-[6rem]">
             Creative
