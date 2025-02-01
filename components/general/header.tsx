9b23d50e1304855ed9c88d18eab112b3e9b2b2e4
Oleksii <alexey.bortnytskyi@gmail.com>
2025-02-01 19:45:32 +0100

    feature(components/general) - implemented <Magnetic> component to <Button> also <href> for current <Button> - is my LinkedIn

--- components/general/header.tsx
+++ components/general/header.tsx
@@ -1,12 +1,35 @@
+import { Magnetic } from "../custom/magnetic";
 import { Button } from "../ui/button";
 
 export const Header = () => {
+  const springOptions = { bounce: 0.1 };
+
   return (
     <header className="container sticky top-2 z-20 mx-auto bg-white bg-opacity-40 backdrop-blur-xl bg-blend-multiply border border-input w-full h-14 p-3.5 rounded-md flex justify-between items-center">
-      <span className="text-sm font-medium font-sans">Bortnytskyi.</span>
-      <Button size="sm" variant="default" className="font-semibold">
-        {`<Contact me/>`}
-      </Button>
+      <span className="text-sm font-medium font-mono">Bortnytskyi.</span>
+
+      <Magnetic
+        intensity={0.2}
+        springOptions={springOptions}
+        actionArea="global"
+        range={200}
+      >
+        <Button
+          href="https://www.linkedin.com/in/bortnytskyi-oleksii/"
+          size="sm"
+          variant="default"
+          className="font-semibold font-mono"
+        >
+          <Magnetic
+            intensity={0.1}
+            springOptions={springOptions}
+            actionArea="global"
+            range={200}
+          >
+            <span>{`<Contact me/>`}</span>
+          </Magnetic>
+        </Button>
+      </Magnetic>
     </header>
   );
 };
