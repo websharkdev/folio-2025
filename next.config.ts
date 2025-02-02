1736e975d0d96401455fe1a4544d8bb79a0c9718
Oleksii <alexey.bortnytskyi@gmail.com>
2025-02-02 14:32:42 +0100

    feature(components/custom) - created new component: <LinkPreview> > shows snapshot of link

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.microlink.io",
+        port: "",
+        pathname: "**",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
